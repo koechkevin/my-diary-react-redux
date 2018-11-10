70113eb015dd34648df863d8b1de72c71bc55fbc
KEVIN KOECH <koechkevin92@gmail.com>
2018-11-10 16:14:47 +0300

    chore(setup): set up front end environment  … - set up css and make it modular - add css dependancies
    [Finishes #161859689]

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,24 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
             }
         ]
     },
