0920942b8161584e4102be0b2e53611f8928152a
KEVIN KOECH <koechkevin92@gmail.com>
2018-11-11 15:52:46 +0300

    revert changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Index = () => <div>Hello React</div>;
+const Index = () => <div id="test">Hello React</div>;
 
 ReactDOM.render(<Index />, document.getElementById('root'));
+export default Index;
